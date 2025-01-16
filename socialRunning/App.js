733042deb2edf1e21711efe0f7848d610a1574e7
Ron Kibel <ron.kibel1@gmail.com>
2025-01-15 20:11:34 -0800

    HelloWorld-lab01

--- socialRunning/App.js
+++ socialRunning/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Ron Kibel's Hello World App</Text>
+      <Text style={styles.bodyText}>This is a test</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  bodyText: {
+    fontSize: 16,
+    color: '#333',
   },
 });
